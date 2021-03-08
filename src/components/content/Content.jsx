74f151939cb1092a93179c11265f323e371dee43
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-08 18:36:44 +0300

    prettify

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Note from './Note'
+import Note from './Note';
 import notes from '../notes.js';
 
 function Content() {
-    return notes.map((note) => <Note id={note.id} key={note.id} title={note.title} content={note.content} />);
+  return notes.map((note) => (
+    <Note id={note.id} key={note.id} title={note.title} content={note.content} />
+  ));
 }
 
 export default Content;
