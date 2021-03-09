18fd5fba61dbe7ed322b6902bc22301f7c616c9b
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-10 02:28:06 +0300

    implement add note

--- src/components/content/Create.jsx
+++ src/components/content/Create.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
-function CreateArea() {
+function Create(props) {
   return (
     <div>
-      <form>
-        <input name='title' placeholder='Title' />
-        <textarea name='content' placeholder='Take a note...' rows='3' />
-        <button>Add</button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          name='title'
+          placeholder='Title'
+          value={props.input.title}
+          onChange={(e) => props.setInput({ ...props.input, title: e.target.value })}
+        />
+        <textarea
+          name='content'
+          placeholder='Take a note...'
+          rows='3'
+          value={props.input.content}
+          onChange={(e) => props.setInput({ ...props.input, content: e.target.value })}
+        />
+        <button type='submit' onClick={() => props.addNote()}>
+          Add
+        </button>
       </form>
     </div>
   );
 }
 
-export default CreateArea;
+export default Create;
--- src/components/content/Notes.jsx
+++ src/components/content/Notes.jsx
@@ -5,21 +5,23 @@ import defaultNotes from '../defaultNotes.js';
 
 function Content() {
   // user input
-  const [input, setInput] = useState({ id: null, title: '', content: '' });
+  const [input, setInput] = useState({ title: '', content: '' });
 
   // all notes
   const [notes, setNotes] = useState(setDefaultNotes());
 
+  // handle add button click
+  function addNote() {
+    const x = getRandomId();
+    setNotes([...notes, { ...input, id: x, key: x }]);
+  }
+
   return (
     <div>
-      <div>
-        <Create />
-      </div>
-      <div>
-        {notes.map((notes) => (
-          <Card id={notes.id} key={notes.id} title={notes.title} content={notes.content} />
-        ))}
-      </div>
+      <Create input={input} setInput={setInput} addNote={addNote} />
+      {notes.map((notes) => (
+        <Card id={notes.id} key={notes.id} title={notes.title} content={notes.content} />
+      ))}
     </div>
   );
 }
@@ -28,6 +30,11 @@ export default Content;
 
 function setDefaultNotes() {
   return defaultNotes.map(function (n) {
-    return { id: Math.random().toString().slice(2), title: n.title, content: n.content };
+    const y = getRandomId();
+    return { key: y, id: y, title: n.title, content: n.content };
   });
+}
+
+function getRandomId() {
+  return Math.random().toString().slice(2);
 }
