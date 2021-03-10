afcacd61f913349f6dde169f777a54af658889a0
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-10 21:40:35 +0300

    add conditional rendering to create note

--- src/components/content/Create.jsx
+++ src/components/content/Create.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
 
 function Create(props) {
+  const [click, setClick] = useState(false);
   return (
     <form className='form-container' onSubmit={(e) => e.preventDefault()}>
       <input
         name='title'
         placeholder='Title'
         value={props.input.title}
+        onClick={() => setClick(true)}
         onChange={(e) => props.setInput({ ...props.input, title: e.target.value })}
       />
-      <textarea
-        name='content'
-        placeholder='Take a note...'
-        rows='3'
-        value={props.input.content}
-        onChange={(e) => props.setInput({ ...props.input, content: e.target.value })}
-      />
-
-      <Zoom in={true}>
-        <Fab
-          className='btn btn-add'
-          style={{ backgroundColor: props.addButtonColor }}
-          type='submit'
-          onClick={props.addNote}
-          aria-label='add'
-        >
-          <i className='fas fa-plus'></i>
-        </Fab>
-      </Zoom>
+      {click && (
+        <textarea
+          name='content'
+          placeholder='Take a note...'
+          rows='3'
+          value={props.input.content}
+          onChange={(e) => props.setInput({ ...props.input, content: e.target.value })}
+        />
+      )}
+      {(props.input.title || props.input.content) && (
+        <Zoom in={true}>
+          <Fab
+            className='btn btn-add'
+            style={{ backgroundColor: props.addButtonColor }}
+            type='submit'
+            onClick={props.addNote}
+            aria-label='add'
+          >
+            <i className='fas fa-plus'></i>
+          </Fab>
+        </Zoom>
+      )}
     </form>
   );
 }
